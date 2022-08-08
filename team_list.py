ae0997ba4ebf40deb1a5917fb4331a91b5bae3ea
colinjclarke <colin@clarkeaviation.com.au>
2022-08-09 09:12:56 +1000

    Create team_list.py
    create initial file

---
+++ team_list.py
@@ -0,0 +1,51 @@
+# create a  list of team members with option to sort or random order after team list created.
+
+print("This will ask you to add team members, then an option to reproduce them in random order")
+print()
+import random
+from datetime import date
+import csv
+# Dictionary
+dt = date.today()
+dt = dt.strftime("%d/%m/%Y")
+teamlist = []
+name = {}
+filename = "C:\\coding data\\Datadump\\team.csv" 
+stopped = False
+# Data capture
+with open(filename, 'a',newline="") as file:
+    csvwriter = csv.writer(file)
+    while not stopped:
+        name = input("add a name, type 'F' - Finish: ")
+        if name == "F":
+            stopped = True
+        elif name == "f":
+            stopped = True    
+        else:
+            csvwriter.writerow([dt, name.title()])  # write as a line to csv file
+            teamlist.append(name.title())
+file.close()  # (save) close file
+# Data count
+team_num = len(teamlist)
+print("\nYou have entered", team_num, "players' names.")
+print("Your team on",dt,"is ready for download.\n")
+request = input("Do you want the team sorted alphabetically Y/N?")
+request = request.upper
+if request == "Y": # Screen print list in alphabetic order
+    teamlist.sort()
+    print("Your team is:")
+elif request == "y": # Screen print list in alphabetic order
+    teamlist.sort()
+    print("Your team is:")
+else:
+    random.shuffle(teamlist)
+    print("Your team in random order is:")
+    
+print(*teamlist, sep= "\n")
+
+
+
+
+
+# create the randon number generator
+# random_num = random.randint(1, team_num)
