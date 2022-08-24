8ee46cc2e6de36d5b28d06d7f84a0c8917942cb0
colinjclarke <colin@clarkeaviation.com.au>
2022-08-24 16:02:10 +1000

    Update weight_conv.py
    Adding name call input

--- weight_conv.py
+++ weight_conv.py
@@ -1,10 +1,15 @@
-weight = float(input("what is your weight:"))
+from ipaddress import collapse_addresses
+
+
+name = input("Hello, what is your name?")
+print("Thanks {}, I will now convert your weight for you.".format(name.title()))
+weight = float(input("What is your weight in pounds or kilograms:"))
 unit = input("(K)g or (L)bs:")
 
 # format vs rounding
 if unit.capitalize() == "K":
     weight = (weight * 2.205)
-    print("Your weight is {:.0f}lbs.".format(weight) )
+    print("Your weight is {:.0f} lbs.".format(weight) )
     # print("Your weight is " + str(round(weight, 0)) + "lbs")
 else:
     weight = (weight / 2.205)
