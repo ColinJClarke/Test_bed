f259f9ffb962c6dfe6192fdea5ee0d21fad3f096
colinjclarke <colin@clarkeaviation.com.au>
2022-09-02 14:08:54 +1000

    Update team_list.py
    Clean up of code to make it neater

--- team_list.py
+++ team_list.py
@@ -1,39 +1,40 @@
 # create a  list of team members with option to sort or random order after team list created.
 
-print("This will ask you to add team members, then an option to reproduce them in random order")
-print()
-import random
+print("This will ask you to add team members, then an option to reproduce them in random order\n")
+
+import random, csv
+import os
 from datetime import date
-import csv
+
 # Dictionary
 dt = date.today()
-dt = dt.strftime("%d/%m/%Y")
+dt = dt.strftime("%d-%b-%Y")
 teamlist = []
 name = {}
-filename = "C:\\coding data\\Datadump\\team.csv" 
+fileDirectory = "C:\\coding data\\Output\\" 
+os.chdir(fileDirectory)
+fileName = "team_list.csv"
 stopped = False
 # Data capture
-with open(filename, 'a', newline="") as file:
+with open(fileName, 'a', newline="") as file:
     csvwriter = csv.writer(file)
     while not stopped:  # see alt below from cat_names.py use while True:
         name = input("add a name, type 'F' - Finish: ")
         #    if name == '':
         # break     alternate way of stopping the loop. 
-        if name == "F":
+        if name.upper() == "F":
             stopped = True
-        elif name == "f":
-            stopped = True    
         else:
             csvwriter.writerow([dt, name.title()])  # write as a line to csv file
             teamlist.append(name.title())
 file.close()  # (save) close file
 # Data count
 team_num = len(teamlist)
-print("\nYou have entered", team_num, "players' names.")
-print("Your team on",dt,"is ready for download.\n")
+print("\nYou have entered {} players' names.".format(team_num))
+print("Your team on {} is ready for download from {}.\n".format(dt, fileDirectory))
 request = input("Do you want the team sorted alphabetically Y/N?")
-request = request.upper
-if request == "Y": # Screen print list in alphabetic order
+
+if request.upper == "Y": # Screen print list in alphabetic order
     teamlist.sort()
     print("Your team is:")
 elif request == "y": # Screen print list in alphabetic order
